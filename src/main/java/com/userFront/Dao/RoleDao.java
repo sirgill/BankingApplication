51d0440bc842a38b85bae3e60a3837ec7b1f64c1
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-04-17 19:10:25 -0400

    Added security Authority, Role, and UserRole.

---
+++ src/main/java/com/userFront/Dao/RoleDao.java
@@ -0,0 +1,5 @@
+package com.userFront.Dao;
+
+public interface RoleDao {
+
+}
--- src/main/java/com/userFront/domain/User.java
+++ src/main/java/com/userFront/domain/User.java
@@ -1,6 +1,9 @@
 package com.userFront.domain;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,139 +15,193 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.userFront.domain.security.Authority;
+import com.userFront.domain.security.UserRole;
 
 @Entity
-public class User {
-	
-	@Id
+public class User implements UserDetails{
+
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "userId", nullable = false, updatable = false)
-	private Long userId;
-	private String username;
-	private String password;
-	private String firstName;
-	private String lastName;
-	private String email;
-	private String phone;
-	
-	private boolean enabled = true;
-	
-	@OneToOne
-	private PrimaryAccount primaryAccount;
-	
-	@OneToOne
-	private SavingsAccount savingAccount;
-	
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Long userId;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    private String phone;
+
+    private boolean enabled=true;
+
+    @OneToOne
+    private PrimaryAccount primaryAccount;
+
+    @OneToOne
+    private SavingsAccount savingsAccount;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
-	private List<Appointment> appointmentList;
-	
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Recipient> recipientList;
+    private List<Appointment> appointmentList;
 
-	public Long getUserId() {
-		return userId;
-	}
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Recipient> recipientList;
 
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<UserRole> userRoles = new HashSet<>();
 
-	public String getUsername() {
-		return username;
-	}
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public Long getUserId() {
+        return userId;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+    public String getPhone() {
+        return phone;
+    }
 
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-	public PrimaryAccount getPrimaryAccount() {
-		return primaryAccount;
-	}
+    public List<Appointment> getAppointmentList() {
+        return appointmentList;
+    }
 
-	public void setPrimaryAccount(PrimaryAccount primaryAccount) {
-		this.primaryAccount = primaryAccount;
-	}
+    public void setAppointmentList(List<Appointment> appointmentList) {
+        this.appointmentList = appointmentList;
+    }
 
-	public SavingsAccount getSavingAccount() {
-		return savingAccount;
-	}
+    public List<Recipient> getRecipientList() {
+        return recipientList;
+    }
 
-	public void setSavingAccount(SavingsAccount savingAccount) {
-		this.savingAccount = savingAccount;
-	}
+    public void setRecipientList(List<Recipient> recipientList) {
+        this.recipientList = recipientList;
+    }
 
-	public List<Appointment> getAppointmentList() {
-		return appointmentList;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setAppointmentList(List<Appointment> appointmentList) {
-		this.appointmentList = appointmentList;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public List<Recipient> getRecipientList() {
-		return recipientList;
-	}
+    public PrimaryAccount getPrimaryAccount() {
+        return primaryAccount;
+    }
 
-	public void setRecipientList(List<Recipient> recipientList) {
-		this.recipientList = recipientList;
-	}
+    public void setPrimaryAccount(PrimaryAccount primaryAccount) {
+        this.primaryAccount = primaryAccount;
+    }
 
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", firstName="
-				+ firstName + ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + ", enabled="
-				+ enabled + "]";
-	}
-	
+    public SavingsAccount getSavingsAccount() {
+        return savingsAccount;
+    }
+
+    public void setSavingsAccount(SavingsAccount savingsAccount) {
+        this.savingsAccount = savingsAccount;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", appointmentList=" + appointmentList +
+                ", recipientList=" + recipientList +
+                ", userRoles=" + userRoles +
+                '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        userRoles.forEach(ur -> authorities.add(new Authority(ur.getRole().getName())));
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
 
 }
